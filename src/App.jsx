6da4c89e1f202e20bdb508bab194347b5c27ac47
Stanislav <stasstrong06@gmail.com>
2025-01-08 14:29:51 +0100

    prepare components for each page, add to App component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import AboutMePage from "./AboutMePage";
+import SkillsPage from "./SkillsPage";
+import ContactsPage from "./ContactsPage";
+
+export default function App() {
+    return (
+        <main>
+            <AboutMePage/>
+            <SkillsPage/>
+            <ContactsPage/>
+        </main>
+    )
+}
