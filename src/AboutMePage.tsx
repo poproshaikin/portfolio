1fb71b51fffadd48a682186f68da1302d4040f1c
Stanislav <stasstrong06@gmail.com>
2025-01-08 14:29:04 +0100

    prepare components for each page, add to App component

--- src/AboutMePage.tsx
+++
@@ -1,9 +0,0 @@
-import "./AboutMePage.css"
-
-export default function AboutMePage() {
-    return (
-        <main>
-            HELLO
-        </main>
-    )
-}
--- src/ContactMePage.tsx
+++
@@ -1,7 +0,0 @@
-export default function ContactMePage() {
-    return (
-        <div>
-
-        </div>
-    )
-}
