709cf85043afbbe2ea6d72ada7fb57c884cf3250
Stanislav <stasstrong06@gmail.com>
2025-01-10 11:23:04 +0100

    navigateTo() now opens urls

--- src/Components/navigationHelper.js
+++ src/Components/navigationHelper.js
@@ -1,9 +1,11 @@
+import { isValidUrl, openUrl } from "./linksHelper";
+
 export function navigateTo(path, navigateFunction) {
     if (path === undefined) {
         return;
-    }
-    if (path.startsWith("#")) {
-        console.log(path)
+    } else if (isValidUrl(path)) {
+        openUrl(path);
+    } else if (path.startsWith("#")) {
         const element = document.getElementById(path.substring(1));
         element.scrollIntoView({ behavior: "smooth" });
     } else {
