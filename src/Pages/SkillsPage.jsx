7f716d57430b359846c659a4019c8e3500b1e2a6
Stanislav <stasstrong06@gmail.com>
2025-01-13 11:48:49 +0100

    cleanse, change some styles

--- src/Pages/SkillsPage.jsx
+++ src/Pages/SkillsPage.jsx
@@ -6,44 +6,38 @@ export default function SkillsPage() {
     return (
         <div id='skl-body'>
             <div id='skl-header-cnt'>
-                <h1>SKILLS</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                <h1>WHAT I EXCEL AT</h1>
+                <p>Turning complex ideas into elegant solutions.</p>
             </div>
             <div id='skl-main-cnt'>
                 <TextBlock height='70vh'>
-                    <div className='skl-col-img-cnt'>
+                    <div className="skl-col-content-cnt">
                         <img src="/assets/dotnet.png" alt="dotnet" />
-                    </div>
-                    <div className='skl-col-text-cnt'>
                         <p>A comprehensive, cross-platform framework that empowers developers to build scalable and high-performance applications. Whether you're creating robust enterprise solutions or modern cloud applications, .NET provides the flexibility and tools needed to turn your ideas into reliable, efficient software.</p>   
+                        <Button width='90%' href='https://dotnet.microsoft.com/en-us/'>
+                            Explore .NET
+                        </Button>
                     </div>
-                    <Button href='https://dotnet.microsoft.com/en-us/'>
-                        Explore .NET
-                    </Button>
                 </TextBlock>
                 <TextBlock height='70vh'>
-                    <div className='skl-col-img-cnt'>
+                    <div className='skl-col-content-cnt'>
                         <img src="/assets/aspnet.png" alt="aspnet" />
-                    </div>
-                    <div className='skl-col-text-cnt'>
                         <p>The framework for building dynamic, high-performance web applications. ASP.NET enables the creation of everything from fast APIs to fully interactive websites, all while supporting cross-platform deployment. With its efficient architecture and flexibility, it ensures that web development remains both powerful and streamlined.</p>
+                        <Button width='90%' href='https://dotnet.microsoft.com/en-us/learn/aspnet/what-is-aspnet'>
+                            Explore ASP.NET
+                        </Button>
                     </div>
-                    <Button href='https://dotnet.microsoft.com/en-us/learn/aspnet/what-is-aspnet'>
-                        Explore ASP.NET
-                    </Button>
                 </TextBlock>
                 <TextBlock height='70vh'>
-                    <div className='skl-col-img-cnt'>
+                    <div className='skl-col-content-cnt'>
                         <img src="/assets/postgres.png" alt="postgres" />
-                    </div>
-                    <div className='skl-col-text-cnt'>
                         <p>A open-source relational database known for its reliability and advanced features. PostgreSQL handles complex queries, large volumes of data, and diverse data types with ease. Its adherence to SQL standards, combined with modern extensions, makes it a go-to choice for businesses and developers demanding consistency and performance.</p>
+                        <Button width='90%' href='https://www.postgresql.org'>
+                            Explore PostgreSQL
+                        </Button>
                     </div>
-                    <Button href='https://www.postgresql.org'>
-                        Explore PostgreSQL
-                    </Button>
                 </TextBlock>
-                <TextBlock height='70vh'>
+                {/* <TextBlock height='70vh'>
                     <div className='skl-col-img-cnt'>
                         <img src="/assets/efcore.png" alt="efcore" />
                     </div>
@@ -53,7 +47,7 @@ export default function SkillsPage() {
                     <Button href='https://learn.microsoft.com/en-us/ef/core/'>
                         Explore EF
                     </Button>
-                </TextBlock>
+                </TextBlock> */}
             </div>
         </div>
     )
