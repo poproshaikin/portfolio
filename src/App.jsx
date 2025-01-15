5bd75c3aaeb3031ace3d3c723bbc4d3acdd4953f
Stanislav <stasstrong06@gmail.com>
2025-01-15 15:46:55 +0100

    import aos library, add separating lines, minor style changings

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import "./App.css"
 import AboutMePage from "./Pages/AboutMePage";
 import SkillsPage from "./Pages/SkillsPage";
+import ProjectsPage from "./Pages/ProjectsPage";
 import ContactsPage from "./Pages/ContactsPage";
-import {Route, Router, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+AOS.init({
+    duration: 1000,
+    offset: 300
+});
 
 export default function App() {
+
     return (
         <main>
-            <Router location='/' navigator='/'>
+            <BrowserRouter location='/' navigator='/'>
                 <Routes>
                     <Route path='/' element={
-                        <div>
+                        <div id='home-cnt'>
                             <AboutMePage/>
+                            <hr className='separating-line' data-aos='fade-up' />
+                            <ProjectsPage/>
+                            <hr className='separating-line' data-aos='fade-up' />
                             <SkillsPage/>
+                            <hr className="separating-line" data-aos='fade-up' />
                             <ContactsPage/>
-                        </div>}
+                        </div>
+                    }
                     />
                 </Routes>
-            </Router>
+            </BrowserRouter>
         </main>
     )
 }
