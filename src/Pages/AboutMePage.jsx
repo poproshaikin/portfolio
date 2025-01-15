e6e8c2b58e82b0702929d6a730f514cd7a3ff2d1
Stanislav <stasstrong06@gmail.com>
2025-01-15 15:42:27 +0100

    Add animation using AOS library

--- src/Pages/AboutMePage.jsx
+++ src/Pages/AboutMePage.jsx
@@ -1,4 +1,4 @@
-import "./AboutMe.css"
+import "./AboutMePage.css"
 import Button from "../Components/Button";
 import TextButton from "../Components/TextButton";
 
@@ -8,25 +8,25 @@ export default function AboutMePage() {
             <img id='abm-sign' src='/assets/podpis.png' alt='podpis'/>
             <div id='abm-main'>
                 <div id='abm-left-part'>
-                    <div id="abm-left-top-cnt">
+                    <div id="abm-left-top-cnt" data-aos='fade-right'>
                         <TextButton content='About Me' href='#abm-main'/>
+                        <TextButton content='Projects' href='#pjs-body'/>
                         <TextButton content='Skills' href='#skl-body'/>
-                        <TextButton content='Contacts' href='#cts-body'/>
+                        <TextButton content='Contacts' href='#cts-body' />
                     </div>
-                    <div id="abm-left-bottom-cnt">
-                        <div id='abm-main-text-cnt'>
+                    <div id="abm-left-bottom-cnt" data-aos='fade-right'>
+                        <div id='abm-main-text-cnt' data-aos='fade-right'>
                             <h1>Hello!</h1>
                             <p>Are you looking for back-end programmer? </p>
                         </div>
-
-                        <div id='abm-skills-btn' >
+                        <div id='abm-skills-btn'>
                             <Button height='8vh' width='15vw' href='#skl-body'>
                                 <p style={{fontSize: "1.7rem"}}>SKILLS</p>
                             </Button>
                         </div>
                     </div>
                 </div>
-                <div id='abm-right-part'>
+                <div id='abm-right-part' >
                     <img id='abm-me-img' src="/assets/ja.png" alt="ja"/>
                 </div>
             </div>
