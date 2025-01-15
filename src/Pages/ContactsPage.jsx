41d4946add9f32f69de9d0c75276f0c531dafb1e
Stanislav <stasstrong06@gmail.com>
2025-01-15 15:42:39 +0100

    Add animation using AOS library

--- src/Pages/ContactsPage.jsx
+++ src/Pages/ContactsPage.jsx
@@ -1,7 +1,13 @@
+import './ContactsPage.css'
+
 export default function ContactsPage() {
+    
     return (
         <div id='cts-body'>
-            Contacts
+            <div id='cts-header-cnt' data-aos='fade-up'>
+                <h1>CONTACT ME</h1>
+                <p>Reach out and say hello!</p>
+            </div>
         </div>
     )
 }
